d3a2ca528966845bbcb3ff1a8c9c60726b3c0538
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-11-22 12:36:31 +0530

    [Dipali] Added : fare for multiple ride

---
+++ src/main/java/com/cabinvoicegenerator/Ride.java
@@ -0,0 +1,12 @@
+package com.cabinvoicegenerator;
+
+public class Ride {
+	public final int time;
+	public double distance;
+
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+
+}
